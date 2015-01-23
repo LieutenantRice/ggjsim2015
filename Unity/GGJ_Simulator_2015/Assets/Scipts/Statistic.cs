782e18522c00924911bda4283d16047705b3bd6c
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-01-24 00:20:45 +0100

    Added basic stats scripts and added VStools to project

---
+++ Unity/GGJ_Simulator_2015/Assets/Scipts/Statistic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Statistic {
+
+  float _minimumValue;
+  float _value;
+  float _maximumValue;
+  float _UpdateRate;
+
+
+  public float Value
+  {
+    get { 
+      return _value; 
+    }
+    set
+    {
+      _value = Mathf.Clamp(value, _minimumValue, _maximumValue);
+    }
+  }
+
+  public void Update()
+  {
+    Value -= _UpdateRate * Time.deltaTime;
+  }
+
+  public Statistic(float minimumValue = 0f, float maximumValue = 1f, float updateRate = -0.05f)
+  {
+    _minimumValue = minimumValue;
+    _maximumValue = maximumValue;
+    _value = maximumValue;
+    _UpdateRate = updateRate;
+  }
+
+}
