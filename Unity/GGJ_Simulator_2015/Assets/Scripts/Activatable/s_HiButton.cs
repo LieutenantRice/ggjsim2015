a6a1b50e8a46966f2482a403e2d4ffe3fafc6fea
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-01-24 10:03:30 +0100

    startup interactable scripts

---
+++ Unity/GGJ_Simulator_2015/Assets/Scripts/Activatable/s_HiButton.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+public class s_HiButton : s_Interactable {
+
+  public override Void Interact()
+  {
+    Debug.Log("hi!");
+  }
+
+}
---
+++ Unity/GGJ_Simulator_2015/Assets/Scripts/Activatable/s_Interactable.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class s_Interactable : MonoBehaviour {
+
+  public abstract void Interact();
+}
