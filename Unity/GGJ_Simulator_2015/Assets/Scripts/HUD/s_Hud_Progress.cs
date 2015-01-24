ac37d7c561b40bbbb1adc672ca44fc8d4d414d90
Pepijn Willekens <pepijnwillekens@gmail.com>
2015-01-24 23:55:59 +0100

    tussen commit

---
+++ Unity/GGJ_Simulator_2015/Assets/Scripts/HUD/s_Hud_Progress.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class s_Hud_Progress : MonoBehaviour
+{
+  public float MaxWidth = 3280;
+  public Text Label;
+  public bool HasLabel;
+  private Statistic _stat;
+	// Use this for initialization
+	void Start ()
+	{
+	  _stat = GameObject.FindGameObjectWithTag("Player").GetComponent<s_Stats>().Stats.progress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal,MaxWidth * _stat.Value);
+    if (HasLabel) Label.text = _stat.Value.ToString("P1");
+	}
+}
