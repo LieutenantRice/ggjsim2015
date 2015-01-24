376e1ebd00e8cc4334147ef9ec2c0a11bb8a50c3
AntonShumin <AntonShuminBe@gmail.com>
2015-01-24 23:55:10 +0100

    Time script

---
+++ Unity/GGJ_Simulator_2015/Assets/Scripts/s_Time.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class s_Time : MonoBehaviour 
+{
+	//public float f_minutes = 2880.0f;
+	public float f_seconds = 172800.0f;
+	public float f_gewensteSpeeltijd = 3;
+
+	void Start () 
+	{
+		f_gewensteSpeeltijd *= 60;
+		f_gewensteSpeeltijd = f_seconds / f_gewensteSpeeltijd; // Verhouding tussen echte en fictieve seconden 
+	}
+
+	void Update () 
+	{
+		float dt = Time.deltaTime;
+		f_seconds -= dt * f_gewensteSpeeltijd;
+	}
+
+	public float getHour()
+	{
+		float hours = f_seconds / 3600;
+		hours = Mathf.Floor (hours);
+		return hours;
+	}
+
+	public float getMinutes()
+	{
+		float minutes = Mathf.Floor ((f_seconds % 3600) / 60);
+	 	return minutes;
+	}
+}
