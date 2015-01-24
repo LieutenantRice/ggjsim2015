410e4a4f116c7fbc51d1bfc7dad5139eb0362a3c
AntonShumin <AntonShuminBe@gmail.com>
2015-01-24 17:08:55 +0100

    Fov run

--- Unity/GGJ_Simulator_2015/Assets/Scripts/Activatable/s_HiButton.cs
+++ Unity/GGJ_Simulator_2015/Assets/Scripts/Activatable/s_HiButton.cs
@@ -4,7 +4,7 @@ using System.Collections;
 
 public class s_HiButton : s_Interactable {
 
-  public override Void Interact()
+  public override void Interact()
   {
     Debug.Log("hi!");
   }
--- Unity/GGJ_Simulator_2015/Assets/Scripts/s_Player.cs
+++ Unity/GGJ_Simulator_2015/Assets/Scripts/s_Player.cs
@@ -8,15 +8,22 @@ public class s_Player : MonoBehaviour
 	public float f_movementSpeed = 3.0f;
 	public float f_mouseSensitivity = 3.0f;
 	public float f_upDownRange = 30.0f;
+    public float f_runSlow = 1.0f;
+    public float f_runMax = 3.0f;
+    public float f_runMultiplier;
+    public float f_velocity = 0.0f;
 
-	private float f_verticleRotation =0;
+	private float f_verticleRotation = 0;
     private CharacterController characterController;
+    private s_Camera s_cam ;
 
 	// Use this for initialization
 	void Start () 
 	{
-		Screen.lockCursor = true;
+        Screen.lockCursor = true;
+        s_cam = Camera.main.GetComponent<s_Camera>();
         characterController = GetComponent<CharacterController>();
+        f_runMultiplier = f_runSlow;
 	}
 	
 	// Update is called once per frame
@@ -31,16 +38,32 @@ public class s_Player : MonoBehaviour
 		Camera.main.transform.localRotation = Quaternion.Euler (f_verticleRotation, 0, 0);
 
 		//Movement
-		float f_forwardSpeed = Input.GetAxis ("Vertical") * f_movementSpeed;
-		float f_sideSpeed = Input.GetAxis ("Horizontal") * f_movementSpeed;
+        if ( Input.GetKey(KeyCode.LeftShift) )
+        {
+            if (f_runMultiplier != f_runMax)
+            {
+                f_runMultiplier = f_runMax;
+                s_cam.Run(true);
+            }
+        }
+        else if (f_runMultiplier != f_runSlow) 
+        {
+            f_runMultiplier = f_runSlow;
+            s_cam.Run(false);
+        }
+		float f_forwardSpeed = Input.GetAxis ("Vertical") * f_movementSpeed * f_runMultiplier;
+        float f_sideSpeed = Input.GetAxis("Horizontal") * f_movementSpeed * f_runMultiplier;
 
 		//Character
 		Vector3 v_speed = new Vector3 (f_sideSpeed, Physics.gravity.y, f_forwardSpeed);
+        
 		v_speed = transform.rotation * v_speed;
 
 		
 
 		characterController.Move ( v_speed * Time.deltaTime );
+        v_speed.y = 0;
+        f_velocity = v_speed.magnitude;
 
 	}
 }
